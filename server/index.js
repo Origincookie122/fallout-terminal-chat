db61a45b0a4dc69fd464550405538a5f3813a7c2
Origincookie122 <83477409+Origincookie122@users.noreply.github.com>
2025-05-16 12:08:47 -0400

    Changed to eventHandler

--- server/index.js
+++ server/index.js
@@ -1,32 +1,26 @@
 const { Server } = require('socket.io');
 
+const handleEvent = require('./utils/eventHandler.js');
+
 const io = new Server({
     cors: {
         origin: ["http://127.0.0.1:5500", "http://localhost", "http://localhost:5500", "http://127.0.0.1:5500/client/n"]
     }
 });
 
-var connectedSockets = [];
+//var connectedSockets = [];
 
 io.on("connection", async (socket) => {
-    connectedSockets[`${socket.id}`] = socket;
+    socket.data.username = "unknown";
+    //connectedSockets[`${socket.id}`] = socket;
 
-    socket.on('setUsername', async(data) => {
-        socket.data.username = data.username;
-        console.log(socket.data.username);
-    })
+    socket.onAny((eventName, ...args) => {
+        handleEvent(io, socket, eventName, ...args);
+    });
 
-    socket.on('message', async(data) => {
-        socket.broadcast.emit('message', data);
-    })
-
-    // setTimeout(() => {
-    //     socket.emit('message', {
-    //         message: "Testing server connection!",
-    //         username: "server",
-    //         withPrefix: true
-    //     })
-    // }, 10000)
+    socket.on('disconnect', () => {
+        console.log(`${socket.data.username} disconnected from the server!`);
+    });
 });
 
 io.listen(3000);
