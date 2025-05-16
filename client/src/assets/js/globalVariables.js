540633d081661116be82c7b806955fc0e2063460
Origincookie122 <83477409+Origincookie122@users.noreply.github.com>
2025-05-16 12:14:17 -0400

    Crediting original terminal ui author

--- client/src/assets/js/globalVariables.js
+++ client/src/assets/js/globalVariables.js
@@ -1,2 +1,3 @@
 const socket = io("127.0.0.1:3000");
 const rootUserName = window.prompt("Please enter your username!");
+window.alert("Credit to: dew01ke / terminalout for terminal ui: https://github.com/dew01ke/terminalout");
